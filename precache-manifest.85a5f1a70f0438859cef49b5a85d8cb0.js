fbf4fc208ef9d99ce03f051b05e0f8b53b40c970
ivb98 <ivanvalette@hotmail.com>
2020-05-08 19:53:24 -0400

    Updates

---
+++ precache-manifest.85a5f1a70f0438859cef49b5a85d8cb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776bb560b26dbf3d55aaf06371722e01",
+    "url": "/react-tailwind-theme-toggler/index.html"
+  },
+  {
+    "revision": "f68e71f2a62db47b942c",
+    "url": "/react-tailwind-theme-toggler/static/css/main.54f21bad.chunk.css"
+  },
+  {
+    "revision": "3c46d5f6aa040ff5eb0e",
+    "url": "/react-tailwind-theme-toggler/static/js/2.c2d4a7d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tailwind-theme-toggler/static/js/2.c2d4a7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f68e71f2a62db47b942c",
+    "url": "/react-tailwind-theme-toggler/static/js/main.4f98fdcc.chunk.js"
+  },
+  {
+    "revision": "9235c02b6ae902776198",
+    "url": "/react-tailwind-theme-toggler/static/js/runtime-main.6734ba75.js"
+  }
+]);
